e95dc7deddb12aded1f2a0edf50205d8616d2c6b
15744 <1574408801@qq.com>
2019-04-01 16:49:01 +0800

    commit

--- src/assets/js/realTimeUrl.js
+++ src/assets/js/realTimeUrl.js
@@ -1,3 +1,3 @@
-const realTimeUrl ="http://47.98.220.131:50902";
-/*const realTimeUrl ="";*/
+/*const realTimeUrl ="http://47.98.220.131:50902";*/
+const realTimeUrl ="";
 export default realTimeUrl;
